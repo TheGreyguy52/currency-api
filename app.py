73abb7b00ae9721d6b8a49e51f1843fd30aa61bf
TheGreyguy52 <markjeffry1231@gmail.com>
2025-07-01 20:08:07 +0800

    Update app.py

--- app.py
+++ app.py
@@ -3,19 +3,14 @@ import requests
 
 app = FastAPI()
 
-API_KEY = "837066195c3e07fb2f8d4e180b95de3"
-
 @app.get("/convert")
 def convert_currency(from_currency: str, to_currency: str, amount: float):
     url = (
         f"https://api.exchangerate.host/convert"
-        f"?from={from_currency}&to={to_currency}&amount={amount}&apikey={API_KEY}"
+        f"?from={from_currency}&to={to_currency}&amount={amount}"
     )
 
     response = requests.get(url)
-
-    print("DEBUG URL:", url)
-    print("DEBUG RESPONSE:", response.text)
 
     if response.status_code != 200:
         raise HTTPException(status_code=500, detail="Currency conversion failed")
